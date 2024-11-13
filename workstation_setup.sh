d2c12ba0111b0dd52cbc3efe2af84764111aa0a4
John McDonald <John.McDonald@mu.ie>
2024-11-13 00:12:23 +0000

    fixed noetic setup -- issues with camera node

--- workstation_setup.sh
+++ workstation_setup.sh
@@ -26,8 +26,12 @@ wstool update
 wstool merge https://raw.githubusercontent.com/RethinkRobotics/sawyer_moveit/melodic_devel/sawyer_moveit.rosinstall
 wstool update
 
-git clone https://github.com/RethinkRobotics/sawyer_simulator.git
-git clone https://github.com/RethinkRobotics-opensource/sns_ik.git -b melodic-devel
+git clone -b noetic_devel https://github.com/RethinkRobotics/sawyer_simulator.git
+git clone -b melodic-devel https://github.com/RethinkRobotics-opensource/sns_ik.git
+
+cd sawyer_simulator
+git apply $SCRIPT_DIR/sawyer_simulator.patch
+cd ..
 
 wstool merge sawyer_simulator/sawyer_simulator.rosinstall
 wstool update
