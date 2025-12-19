338e1c35f860bffc0a54431372e5231682398a09
John McDonald <John.McDonald@mu.ie>
2025-12-19 11:24:06 +0000

    added docker network setup + realsense packages

--- run_command.sh
+++ run_command.sh
@@ -15,16 +15,17 @@ do
 done
 
 docker run $DAEMON --rm \
---network host --privileged \
---volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
--e DISPLAY \
--v /dev:/dev \
---workdir="/home/$USER/ros_sawyer" \
---volume="$ROS_SAWYER_DIR:/home/$USER/ros_sawyer" \
---user $UID:$GID \
---volume="/etc/group:/etc/group:ro" \
---volume="/etc/passwd:/etc/passwd:ro" \
---volume="/etc/shadow:/etc/shadow:ro" \
---volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
---name='ros' sawyer-intel:latest "$@"
+	--net=robot_net \
+	--privileged \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	-e DISPLAY \
+	-v /dev:/dev \
+	--workdir="/home/$USER/ros_sawyer" \
+	--volume="$ROS_SAWYER_DIR:/home/$USER/ros_sawyer" \
+	--user $UID:$GID \
+	--volume="/etc/group:/etc/group:ro" \
+	--volume="/etc/passwd:/etc/passwd:ro" \
+	--volume="/etc/shadow:/etc/shadow:ro" \
+	--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+	--name='ros' sawyer-intel:latest "$@"
 
